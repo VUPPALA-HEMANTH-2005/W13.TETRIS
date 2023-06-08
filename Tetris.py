0106adfaebe9e5248335a340556ae8b101ced9ff
Gampa-Abhinay2005 <122970004+Gampa-Abhinay2005@users.noreply.github.com>
2023-06-08 20:02:51 +0530

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -82,20 +82,4 @@ colors = [
     (204,204,0),
     (179,26,255),
 ]
-#Creating a class for generating different figures
-class Figure:
 
-#postion of the top left corner
-    p = 0
-    q = 0
-
-    different_shapes = [
-        [[3,7,11,15], [12,13,14,15]], # staright line
-        [[4, 5, 11, 10], [2, 6, 13, 9]], # Different Z shapes
-        [[6, 7, 9, 8], [1, 5, 14, 10]],# Different reverse Z shapes
-        [[3,2,6,10], [8,12,13,14], [3,7,11,10], [9,10,11,15]],# different L shapes
-        [[0,1,5,9], [11,10,9,13], [0,4,8,9], [11,15,14,13]],# different 7 shapes
-        [[10,13,14,15], [3,7,6,11], [1,2,3,6], [6,10,11,14]],# different T shapes
-        [[10,11,14,15]],# square
-        [[4,0,1,2,6],[0,1,5,8,9],[0,4,5,6,2],[1,0,4,8,9]]# differnt U shapes
-    ]
